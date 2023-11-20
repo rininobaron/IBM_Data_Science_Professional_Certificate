b5adca09e13c6e1d4e362d3487dc4359cd129d00
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-11-20 05:48:21 -0600

    Second answer Task 4

--- COURSE_8_Data_Visualization_with_Python/Week4/LAB3/dash_interactivity_barplot.py
+++ COURSE_8_Data_Visualization_with_Python/Week4/LAB3/dash_interactivity_barplot.py
@@ -13,3 +13,23 @@ airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.
                             encoding = "ISO-8859-1",
                             dtype={'Div1Airport': str, 'Div1TailNum': str, 
                                    'Div2Airport': str, 'Div2TailNum': str})
+
+# Create a dash application layout
+app = dash.Dash(__name__)
+
+# Get the layout of the application and adjust it.
+# Create an outer division using html.Div and add title to the dashboard using html.H1 component
+# Add a html.Div and core input text component
+# Finally, add graph component.
+app.layout = html.Div(children=[html.H1('Total number of flights to the destination state split by reporting air',style={'textAlign': 'center', 'color': '#503D36', 'font-size': 50}),
+                                html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010', 
+                                type='number', style={'height':'50px', 'font-size': 35}),], 
+                                style={'font-size': 40}),
+                                html.Br(),
+                                html.Br(),
+                                html.Div(dcc.Graph(id='bar-plot')),
+                                ])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server()
