3f8a5c3d059cd75b1a5350a3736541992c2fc310
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-11-20 02:18:16 -0600

    Perform TASK 1 - Read the data~

---
+++ COURSE_8_Data_Visualization_with_Python/Week4/LAB3/dash_interactivity.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+# Import required libraries
+import pandas as pd
+import plotly.graph_objects as go
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+
+# Read the airline data into the pandas dataframe
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+print(airline_data.head())
