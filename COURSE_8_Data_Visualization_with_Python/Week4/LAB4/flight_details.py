bb1e4426f552a0b6737dfc2f6793b55655601acd
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-11-20 19:38:59 -0600

    Perform TASK 1 - Read the data

---
+++ COURSE_8_Data_Visualization_with_Python/Week4/LAB4/flight_details.py
@@ -0,0 +1,16 @@
+# Import required libraries
+import pandas as pd
+import plotly.graph_objects as go
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+print(airline_data)
