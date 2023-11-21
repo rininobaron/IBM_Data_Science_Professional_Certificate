76c1d9d2548256dce045cf3476236927f16d774c
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-11-20 20:13:41 -0600

    Perform TASK 2 - Create dash application and get the layout skeleton

--- COURSE_8_Data_Visualization_with_Python/Week4/LAB4/flight_details.py
+++ COURSE_8_Data_Visualization_with_Python/Week4/LAB4/flight_details.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 # Import required libraries
 import pandas as pd
 import plotly.graph_objects as go
@@ -13,4 +15,28 @@ airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.
                             dtype={'Div1Airport': str, 'Div1TailNum': str, 
                                    'Div2Airport': str, 'Div2TailNum': str})
 
-print(airline_data)
+# Create a dash application
+app = dash.Dash(__name__)
+
+# Build dash app layout
+app.layout = html.Div(children=[ html.H1(),
+                                html.Div(["Input Year: ", dcc.Input()],
+                                style={'font-size': 30}),
+                                html.Br(),
+                                html.Br(), 
+                                html.Div([
+                                        html.Div(),
+                                        html.Div()
+                                ], style={'display': 'flex'}),
+    
+                                html.Div([
+                                        html.Div(),
+                                        html.Div()
+                                ], style={'display': 'flex'}),
+                                
+                                html.Div(, style={'width':'65%'})
+                                ])
+
+# Run the app
+if __name__ == '__main__':
+       app.run_server()
